031c61154a9bbe679029ab35dc4add0340db9559
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-24 22:16:02 +0100

    Java Syntax Exercise02

---
+++ 00-Java-Tutorial/00-Java-Syntax/Exercise02.java
@@ -0,0 +1,15 @@
+public class Exercise02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+        // This is a single-line comment
+/* This
+is
+a
+multi-line
+comment
+ */
+
+}
+
