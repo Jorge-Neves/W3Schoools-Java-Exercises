a02c6bb079e0f6d595dba3f7844ee0927c103d5c
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-25 00:04:37 +0100

    Java Variables Exercise04

---
+++ 00-Java-Tutorial/01-Java-Variables/Exercise04.java
@@ -0,0 +1,8 @@
+public class Exercise04 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+        }
+}
