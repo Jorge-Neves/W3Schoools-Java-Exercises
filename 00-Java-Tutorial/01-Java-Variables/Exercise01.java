e7662e7a6eb80d124317c761494dd9d6e8fe4aed
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-24 22:40:24 +0100

    Python Variables Exercise 01 and 02

---
+++ 00-Java-Tutorial/01-Java-Variables/Exercise01.java
@@ -0,0 +1,6 @@
+public class Exercise01 {
+    public static void main(String[] args) {
+        String carName = "Volvo";
+        System.out.println(carName);
+    }
+}
---
+++ 00-Java-Tutorial/01-Java-Variables/Exercise02.java
@@ -0,0 +1,6 @@
+public class Exercise02 {
+    public static void main(String[] args) {
+        int maxSpeed=120;
+        System.out.print(maxSpeed);
+    }
+}
