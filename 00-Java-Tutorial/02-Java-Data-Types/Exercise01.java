06998133d9e14eb37a56f0a8f681894343efbab3
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-25 03:20:42 +0100

    Java Data Types Exercise 01 and 02

---
+++ 00-Java-Tutorial/02-Java-Data-Types/Exercise01.java
@@ -0,0 +1,14 @@
+public class Exercise01 {
+    public static void main(String[] args) {
+        int myNum = 9;
+        float myFloatNum = 8.99f;
+        char myLetter = 'A';
+        boolean myBool = false;
+        String myText = "Hello World";
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
---
+++ 00-Java-Tutorial/02-Java-Data-Types/Exercise02.java
@@ -0,0 +1,21 @@
+public class Exercise02 {
+    public static void main(String[] args){
+        byte myByte = 1;
+        short myShort = 32;
+        int myInteger = 20;
+        long myLong = 10000L;
+        float myFloat = 3.44f;
+        double myDouble = 33.44d;
+        boolean myBool = false;
+        char myLetter = 'D';
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInteger);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myBool);
+        System.out.println(myLetter);
+        // All of these variables are of primitive data types
+    }
+}
