96df439d5eefd5c0c9aa072666a4e58b0c523318
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-25 03:23:44 +0100

    Java Data Types Exercise03

---
+++ 00-Java-Tutorial/02-Java-Data-Types/Exercise03.java
@@ -0,0 +1,7 @@
+public class Exercise03 {
+    public static void main(String[] args){
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
+    }
+}
