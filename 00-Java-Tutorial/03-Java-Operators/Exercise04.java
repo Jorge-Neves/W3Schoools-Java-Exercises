79e4e051ca9189d0c78371f207843d92c431d5a6
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-25 17:31:16 +0100

    Java Operators update Exercise04

--- 00-Java-Tutorial/03-Java-Operators/Exercise04.java
+++ 00-Java-Tutorial/03-Java-Operators/Exercise04.java
@@ -1,6 +1,7 @@
 public class Exercise04 {
     public static void main(String[] args){
         int x = 10;
-        System.out.println(x + 5);
+        x += 5;
+        System.out.println(x);
     }
 }
